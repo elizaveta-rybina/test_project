8aca60bdb7b9bf96eef24df2eb94bb97cd8c6a4d
elizavetarybina <liza.rybina@inbox.ru>
2025-02-05 01:14:51 +0300

    #feat: добавлены стили и уведомления

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Main } from 'components/Main'
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import styles from './App.module.scss'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.appContainer}>
+      <Toaster position="top-right" />
+      <Main />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
