13ccc005ef4e5ffffca34fd375203cf3f80c5cfd
elizavetarybina <liza.rybina@inbox.ru>
2025-02-05 01:15:48 +0300

    #feat: добавлено хранение данных

--- src/components/FileUploader/FileUpload.tsx
+++ src/components/FileUploader/FileUpload.tsx
@@ -1,36 +1,45 @@
+import { setFileData } from 'app/fileSlice'
 import React from 'react'
 import toast from 'react-hot-toast'
+import { useDispatch } from 'react-redux'
 import { convertXLSXToJSON } from 'shared'
 import styles from './FileUpload.module.scss'
 
+
 export const FileUpload: React.FC = () => {
+  const dispatch = useDispatch()
+
   const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+    const file = event.target.files?.[0]
 
     if (!file) {
-      toast.error('Файл не выбран');
-      return;
+      toast.error('Файл не выбран')
+      return
     }
 
     try {
-      const jsonData = await convertXLSXToJSON(file);
-      console.log('Конвертированные данные:', jsonData);
-      toast.success('Файл успешно загружен и готов к использованию');
+      const jsonData = await convertXLSXToJSON(file)
+      dispatch(setFileData(jsonData))
+      toast.success('Файл успешно загружен и сохранен в сторе')
     } catch (error) {
-      console.error('Ошибка при конвертации файла:', error);
-      toast.error('Ошибка при загрузке файла');
+      console.error('Ошибка при конвертации файла:', error)
+      toast.error('Ошибка при загрузке файла')
     }
-  };
+  }
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Конвертируйте файл</h1>
+      <label htmlFor="file-upload" className={styles.fileInputLabel}>
+        Выберите файл
+      </label>
       <input
+        id="file-upload"
         type="file"
         accept=".xlsx, .xls"
         onChange={handleFileUpload}
         className={styles.fileInput}
       />
     </div>
-  );
-};
+  )
+}
